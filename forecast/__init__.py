dd7fae513ed8edca4e3d605dae77eb72cac2a36c
kaija <kaija.chang@gmail.com>
2020-03-23 16:34:56 +0800

    add python package template

---
+++ forecast/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of forecast.
+# https://github.com/kaija/forecast.git
+
+# Licensed under the MIT license:
+# http://www.opensource.org/licenses/MIT-license
+# Copyright (c) 2020, Kaija <kaija.chang@gmail.com>
+
+from forecast.version import __version__  # NOQA
